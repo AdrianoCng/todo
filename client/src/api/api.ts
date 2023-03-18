7b51b90a59a95fb2bfe82604a8197464da5f09e4
Adriano Cangiamila <adrianocangiamila@gmail.com>
2023-03-18 12:08:01 +0000

    edit scripts

--- client/src/api/api.ts
+++ client/src/api/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "http://ec2-18-133-79-56.eu-west-2.compute.amazonaws.com:5000",
+    baseURL: "https://todo-app-123.herokuapp.com",
     timeout: 10000,
 });
