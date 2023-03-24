9b8e5c277fdb722a9cd7ebcec1d43e782fb813ef
Adriano Cangiamila <adrianocangiamila@gmail.com>
2023-03-24 20:43:52 +0000

    change baseURL

--- client/src/api/api.ts
+++ client/src/api/api.ts
@@ -6,7 +6,7 @@ import { logout } from "../utils";
 export const baseURL =
     process.env.NODE_ENV === "development"
         ? "http://localhost:5000/api/v1"
-        : "https://todo-app-123.herokuapp.com/api/v1";
+        : "https://todo-app-express.herokuapp.com/api/v1";
 
 const api = axios.create({
     baseURL,
